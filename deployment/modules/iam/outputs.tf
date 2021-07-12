4b7fda71def60630b8caa47f2f8206eae01e4145
Ishan <ishan@one2n.in>
2021-07-12 12:36:41 +0530

    Remove secrets output for iam users

--- deployment/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "iam_user_s3_docker_registry_editor_secret" {
-  description = "Secret for IAM user - s3_docker_registry_editor"
-  value       = aws_iam_access_key.s3_docker_registry_editor.secret
-}
-
-output "iam_user_s3_docker_registry_reader_secret" {
-  description = "Secret for IAM user - s3_docker_registry_reader"
-  value       = aws_iam_access_key.s3_docker_registry_reader.secret
-}
